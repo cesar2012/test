373ef4f5280397ad099faffbc00a693e881f748e
cesar2012 <ceanbumo@gmail.com>
2017-11-29 14:29:50 -0300

    Actualización chequeo de roles, página acceso denegado

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
 
 use Closure;
 
@@ -15,14 +16,41 @@ class CheckRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $role)
     {
         
-    Log::info('Pasando por aqui ');
+        //Se podría validar por grupos de roles con arreglos
 
-        if ( $request->user()->role == "standar") {
-         return redirect('/'); //Debería ser una página llamada accedo denegado
-        }    	
-        return $next($request);
+        $agrega_o_quita = substr($role, 0,1);
+        $contenido = substr($role, 1);
+       // Log::info('Pasando por aqui ' . $agrega_o_quita .   $contenido );
+
+
+        if ($agrega_o_quita == "-") { // quita a rol específcio el acceso a la ruta
+
+            if ( $request->user()->role == "$contenido" ) {  // aquí se podría colocar un arreglo
+                return Response::view('denegado');
+
+            }  else{
+                return $next($request);
+            }        
+        }
+
+
+        if ($agrega_o_quita == "s") { // quita a todos menos al rol. sólo el rol
+
+            if ( $request->user()->role == "$contenido" ) {  // aquí se podría colocar un arreglo
+                return $next($request);            
+            } else {
+                return Response::view('denegado');
+            }         
+        }
+   	
+       
     }
 }
+
+
+// Está en modalidad permisivo. Por defecto todos los roles se permiten. Luego se quitan accesos a algunos
+// - para quitar
+// s sólo el rol. Es para quitar a todos el acceso menos al rol
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 Route::get('productos',	'ProductoController@index')->middleware('auth');
 Route::get('productos/index',	'ProductoController@index')->name('productos.index');
-Route::get('productos/create',	'ProductoController@create')->name('productos.create')->middleware('role');
+Route::get('productos/create',	'ProductoController@create')->name('productos.create')->middleware('role:-standar');
 Route::post('productos/store', 'ProductoController@store')->name('productos.store');
 Route::get('productos/show/{id}', 'ProductoController@show')->name('productos.show');
 Route::get('productos/edit/{id}', 'ProductoController@edit')->name('productos.edit');
