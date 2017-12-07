6a1fa4d6d35527bd17c83f77a60b275d4499ce0e
cocochkie <rol.molina.s@gmail.com>
2017-12-07 17:29:17 -0300

    prueba commit 3 en branch de prueba
    se intententa guardar los cambios con un branch diferente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
             <div class="flex-center position-ref ">
  <div class="title m-b-md">
-        Gestor de Máquinas y Arriendos
+        Gestor de Máquinas y Arriendos y este si se a hackeado
 </div>
 </div>
 @endsection  
