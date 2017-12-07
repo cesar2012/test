a763ecc353eeda867d01764c23ab254c34636469
cesar2012 <ceanbumo@gmail.com>
2017-12-07 17:37:42 -0300

    Revert "prueba de commit"
    This reverts commit 638d135e27254dc33e8a5ede7e7bd2138a8179a3.
    
    Fue un error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
             <div class="flex-center position-ref ">
  <div class="title m-b-md">
-        Gestor de Máquinas y Arriendos y asjdhakjshd
+        Gestor de Máquinas y Arriendos
 </div>
 </div>
 @endsection  
